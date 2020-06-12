cc02a8c815e2cf027a75fb1260b5fbef57d62a0e
Md Rakibul Islam <55613010+rakibgiant@users.noreply.github.com>
2020-06-13 03:26:58 +0800

    Query goods' item price
    Analysis loop condition and loop operations
         	loop condition:Type n” to end the loop
    	loop operations:Enter the item number and display the corresponding item price
    2. Write the program according to while syntax
    
    Use switch in a loop

---
+++ QuariGoodsPrice.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class QuariGoodsPrice {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    char no = 'n';
+    System.out.println("My shopping manegment systeam > Checkout");
+    System.out.println(
+      "**********************************************************************************************\n"
+    );
+    System.out.println("Please Select the item for purchase: ");
+    System.out.println("1.T-shart \t2.Tennis shoes \t3.Tennis Racket");
+    System.out.println(
+      "**********************************************************************************************\n"
+    );
+    char chose2 = 'y';
+    while (chose2 != no) {
+      System.out.println("Please entre the item number: ");
+      int chiose = input.nextInt();
+      switch (chiose) {
+        case 1:
+          System.out.println("T-shart : $100.0");
+          break;
+        case 2:
+          System.out.println("Tennis shoes : $200.0");
+          break;
+        case 3:
+          System.out.println("Tennis Racket : $300.0");
+          break;
+      }
+      System.out.println("Conteniue or not(y/n): ");
+      chose2 = input.next().charAt(0);
+    }
+  }
+}
