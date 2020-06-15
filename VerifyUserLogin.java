e598a7c400d38d05447ba0b7cae3fddcce276a78
Md Rakibul Islam <55613010+rakibgiant@users.noreply.github.com>
2020-06-16 03:28:56 +0800

    verify user id password
    There are repeation operations and the number of repeation is fixed.
    Enter 3 members' information in a loop.
    if member number is invalid，use continue  statement to jump the program.

---
+++ VerifyUserLogin.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VerifyUserLogin {
+    public static void main(String[] args) {
+        String user_name = "Devid";
+        int user_password = 72542;
+        int chance = 3;
+        Scanner input = new Scanner(System.in);
+        while(chance>0){
+          System.out.print("Username: ");
+          String name = input.nextLine();
+          System.out.print("Password: ");
+          int password = input.nextInt();
+          input.nextLine();
+          if(user_password==password || user_name==name){
+              System.out.println("Welcome to my Shoping System!!!!");
+              break;
+          }else{
+              chance--;
+              System.out.println("Type error!!! U have "+chance+" chance left.");
+          }
+        }
+        if(chance==0){
+        System.out.println("Sorry U have type error 3 times!! Try again later ");
+    }}
+}
