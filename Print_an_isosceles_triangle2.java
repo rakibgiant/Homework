a3df1fd74590fd2e88f62521f3510f4f2ed7766b
Md Rakibul Islam <55613010+rakibgiant@users.noreply.github.com>
2020-06-17 09:20:21 +0800

    Print an isosceles triangle

---
+++ Print_an_isosceles_triangle2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Print_an_isosceles_triangle2 {
+    public static void main(String[] args) {
+        int i, j, n;
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many row u want: ");
+    n = input.nextInt();
+
+    for (i = 1; i <= n; i++) {
+      for (j = i; j <= n; j++) {
+        System.out.print("#");
+      }
+      for (j = 1; j <= i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println("");
+    }
+    }
+}
