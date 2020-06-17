4b8a886245e7feccc18039ba780c41ef32d2ab5f
Md Rakibul Islam <55613010+rakibgiant@users.noreply.github.com>
2020-06-17 09:44:10 +0800

    Print the multiplication table
    1.two-level nested loops are used to realize multiplication table
    2.The multiplication table has 9 rows, and the outer loop condition is
                i<=9
    There are i  equations on the ith row, and the inner loop condition is
    	       j <= i
    The  jth expression on the ith row is
                j's value  * i's value = j*i 's value

---
+++ MultiplicationTable_2.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable_2 {
+   public static void main(String[] args) {
+       int i,j;
+    for(i=1;i<=9;i++){
+        for(j=1;j<=i;j++){
+            System.out.print(j+"*"+i+"="+(j*i)+"  ");
+        }
+        System.out.println(" ");
+    }
+   } 
+}
